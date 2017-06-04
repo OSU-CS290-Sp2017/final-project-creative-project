4fce7a984789a3411994b2806bd9f4d8ab2b0363
plavabe12 <alathare@oregonstate.edu>
2017-06-04 01:17:24 -0700

    Added a few Changes to the entire project and website

---
+++ index.js
@@ -0,0 +1,37 @@
+var img = document.getElementsByTagName('img');
+var popups = document.getElementsByClassName('popup');
+
+img[0].onmouseover = function() {
+  popups[0].style.display = 'block';
+}
+img[0].onmouseout = function() {
+  popups[0].style.display = 'none';
+}
+
+img[1].onmouseover = function() {
+  popups[1].style.display = 'block';
+}
+img[1].onmouseout = function() {
+  popups[1].style.display = 'none';
+}
+
+img[2].onmouseover = function() {
+  popups[2].style.display = 'block';
+}
+img[2].onmouseout = function() {
+  popups[2].style.display = 'none';
+}
+
+img[3].onmouseover = function() {
+  popups[3].style.display = 'block';
+}
+img[3].onmouseout = function() {
+  popups[3].style.display = 'none';
+}
+
+img[4].onmouseover = function() {
+  popups[4].style.display = 'block';
+}
+img[4].onmouseout = function() {
+  popups[4].style.display = 'none';
+}
