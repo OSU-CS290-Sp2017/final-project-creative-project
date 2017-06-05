acd8b616a4869b1c7d3ea143fba433f1a06a435c
AdamRuark <adamruark15@gmail.com>
2017-06-05 11:39:20 -0700

    Added some of the dynamic content

---
+++ server.js
@@ -0,0 +1,25 @@
+var path = require("path");
+var express = require("express");
+var exphbs = require("express-handlebars");
+var cardinfo = require("./info.json");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine("handlebars", exphbs());
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.get('/', function (req, res, next) {
+	var args = {
+		galleries: cardinfo,
+		title: "Photography Galleries",
+		navbar: "Add Gallery"
+	};
+	res.status(200);
+	res.render('homepage', args);
+})
+
+app.listen(port, function(){
+	console.log("Server has started on port: " + port + "\n");
+})
