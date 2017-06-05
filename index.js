bf5ecacfcbe7724dd3333416620b40d1719ad75b
AdamRuark <adamruark15@gmail.com>
2017-06-04 19:22:59 -0700

    Added event listeners via loop

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
 var img = document.getElementsByTagName('img');
 var popups = document.getElementsByClassName('popup');
 
-img[0].onmouseover = function() {
-  popups[0].style.display = 'block';
-}
-img[0].onmouseout = function() {
-  popups[0].style.display = 'none';
-}
 
-img[1].onmouseover = function() {
-  popups[1].style.display = 'block';
-}
-img[1].onmouseout = function() {
-  popups[1].style.display = 'none';
-}
+for(var i = 0; i < img.length; i++){
+  (function(popelem){
+    img[i].onmouseover =  function(){
+      popelem.style.display = 'block';
+    };
+  }) (popups[i]);
 
-img[2].onmouseover = function() {
-  popups[2].style.display = 'block';
-}
-img[2].onmouseout = function() {
-  popups[2].style.display = 'none';
-}
-
-img[3].onmouseover = function() {
-  popups[3].style.display = 'block';
-}
-img[3].onmouseout = function() {
-  popups[3].style.display = 'none';
-}
-
-img[4].onmouseover = function() {
-  popups[4].style.display = 'block';
-}
-img[4].onmouseout = function() {
-  popups[4].style.display = 'none';
+  (function(popelem){
+  img[i].onmouseout =  function(){
+      popelem.style.display = 'none';
+    };
+  }) (popups[i]);
 }
