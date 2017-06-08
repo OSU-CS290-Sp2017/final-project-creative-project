8909efb258c520cb1c62d7da783ee7a5517de72d
AdamRuark <adamruark15@gmail.com>
2017-06-07 18:04:58 -0700

    testing

--- server.js
+++ server.js
@@ -10,17 +10,41 @@ app.engine("handlebars", exphbs());
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', function (req, res, next) {
 	var args = {
 		galleries: cardinfo,
 		title: "Photography Galleries",
-		navbar: "Add Gallery"
+		navbar: "Add Gallery",
+		navbarLink: "#"
 	};
 	res.status(200);
 	res.render('homepage', args);
 })
+
+app.get('/gallery/:page', function(req, res, next){
+
+	var page = req.params.page;
+	var singleCard = cardinfo[page-1];
+
+	console.log(singleCard.gallery);
+
+	if(page > cardinfo.length || page < 0 || page == "-0"){
+		next();
+	}
+	else{
+		var args = {
+			galleries: singleCard, //change to actual images in gallery
+			title: singleCard["gallery"],
+			navbar: "By " + singleCard.author,
+			navbarLink: "#"
+		};
+		res.status(200);
+		res.render('gallerypage', args);
+	}
+})
+
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.listen(port, function(){
 	console.log("Server has started on port: " + port + "\n");
