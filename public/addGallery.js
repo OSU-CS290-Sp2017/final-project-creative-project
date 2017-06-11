52385b1a5dc4932bf63310ab938116ec24190384
AdamRuark <adamruark15@gmail.com>
2017-06-10 17:42:22 -0700

    Changed layout of add gallery form. Added a 'remove link' button

--- public/addGallery.js
+++ public/addGallery.js
@@ -1,17 +1,50 @@
-var button = document.getElementById("button");
-var textfield = document.getElementById("imageurl1");
-var list = document.getElementById("list");
+var addButton = document.getElementById("addbutton");
+var removeButton = document.getElementById("removebutton");
+var tableBody = document.getElementById("newinput");
+var table = document.getElementsByTagName("table");
 
-button.addEventListener('click', function() {
-	var value = textfield.value;
-	if(value){
-		var node = document.createElement("li");
-		var textnode = document.createTextNode(value);
-		node.appendChild(textnode);
-		list.appendChild(node);
-		textfield.value = '';
-	}
-	else{
-		alert("Please enter a URL");
+/*
+<tr>
+	 <td class="col1"><label for="addtionalimgs" class="name">Addtional Image URL: </label></td>
+	 <td class="col2"><br><input type="url" class="col2" id="addtionalimgs" name="addtionalimgs" placeholder="Enter a URL"></input></br></td>
+</tr>
+*/
+
+addbutton.addEventListener('click', function() {
+	var tr = document.createElement("tr");
+
+	var td1 = document.createElement("td");
+	var td2 = document.createElement("td");
+
+	td1.classList.add("col1");
+	td2.classList.add("col2");
+
+	var label = document.createElement("label");
+	var textnode = document.createTextNode("Image");
+
+	label.for = "addtionalimgs";
+	label.classList.add("name");
+	label.appendChild(textnode);
+
+	td1.appendChild(label);
+
+	var input = document.createElement("input");
+	input.type = "url";
+	input.classList.add("col2");
+	input.id = "addtionalimgs";
+	input.name = "addtionalimgs";
+	input.placeholder = "Enter a URL";
+
+	td2.appendChild(input);
+
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+
+	tableBody.after(tr);
+})
+
+removeButton.addEventListener('click', function(){
+	if(table[0].rows.length > 4){
+		table[0].deleteRow(4);
 	}
 })
